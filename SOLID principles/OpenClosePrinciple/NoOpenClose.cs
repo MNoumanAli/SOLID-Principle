5af3fa0e094b7128a9de75b937124e0846dfb04f
MNoumanAli <mnoumanali001@gmail.com>
2024-11-04 12:52:50 +0500

    implement open-close principle

---
+++ SOLID principles/OpenClosePrinciple/NoOpenClose.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_principles.OpenClosePrinciple
+{
+    public abstract class NoOpenClose
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Salary { get; set; }
+
+        public NoOpenClose(string name, string description, int salary)
+        {
+            Name = name;
+            Description = description;
+            Salary = salary;
+        }
+
+        public int calculateBonus()
+        {
+            return 200 * Salary;
+            /*
+             * Let say our requirements changes for caclualte bonus w.r.t employee type then we have
+             * to modify this function so this will voilate the principle as it is not close for modification
+             */
+        }
+    }
+}
---
+++ SOLID principles/OpenClosePrinciple/OpenClose.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_principles.OpenClosePrinciple
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string name, string description, int salary)
+        {
+            Name = name;
+            Description = description;
+            Salary = salary;
+        }
+
+        public abstract int calculateBonus();
+    };
+
+    public class PermanentEmployee : Employee
+    {
+        public int Bonus { get; set; }
+        public PermanentEmployee(string name, string description, int salary, int bonus) : base( name, description, salary)
+        {
+            Bonus = bonus;
+        }
+        public override int calculateBonus()
+        {
+            return Bonus * Salary;
+        }
+    }
+
+}
