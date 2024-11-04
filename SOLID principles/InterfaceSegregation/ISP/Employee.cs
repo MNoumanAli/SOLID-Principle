f8cb4ae2d0180a59ba17b3179da41a16379e0f11
MNoumanAli <mnoumanali001@gmail.com>
2024-11-04 13:08:01 +0500

    implement interface segeration principle

---
+++ SOLID principles/InterfaceSegregation/ISP/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_principles.InterfaceSegregation.ISP
+{
+    public interface IEmployee
+    {
+        public bool login(string username, string password);
+        public bool register(string username, string email, string password);
+    }
+}
---
+++ SOLID principles/InterfaceSegregation/NoInterfaceSegregation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_principles.InterfaceSegregation
+{
+    public interface IEmployee
+    {
+        public bool login(string username, string password);
+        public bool register(string username, string email, string password);
+        public void sendEmail();
+        public void logger();
+    }
+}
